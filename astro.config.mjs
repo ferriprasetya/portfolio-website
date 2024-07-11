fa09bfbf4642b94edb1a8fa895c72b013bec95f7
ferriprasetya <feriadiprasetya12@gmail.com>
2024-07-11 23:31:44 +0700

    feat/seo: add meta tags and setup sitemap to provide seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import icon from 'astro-icon'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ferriadi.my.id',
   integrations: [
     tailwind(),
     icon({
       iconDir: 'src/assets/icons',
     }),
+    sitemap(),
   ],
 })
